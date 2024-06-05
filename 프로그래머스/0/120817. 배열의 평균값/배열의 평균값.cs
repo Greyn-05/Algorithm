b3f177b59acda982f42d85d5f6da9d700ff17ede
Greyn-05 <163137946+Greyn-05@users.noreply.github.com>
2024-06-05 18:43:40 +0900

    [level 0] Title: 배열의 평균값, Time: 0.87 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public double solution(int[] numbers)
+    {
+        double answer = numbers.Average();
+        return answer;
+    }
+}
