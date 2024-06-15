8ffc753317e4e36f253a1206c3beb5624bb17083
Greyn-05 <163137946+Greyn-05@users.noreply.github.com>
2024-06-15 21:58:56 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.59 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public string solution(string my_string, int k)
+    {
+        string answer = string.Concat(Enumerable.Repeat(my_string, k));
+        return answer;
+    }
+}
